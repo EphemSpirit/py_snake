fe29dfe063658f4ebc1c44dc80a92c0244844959
Drew Hund <languageinvestigator@gmail.com>
2026-03-23 13:33:01 -0400

    snake animation basics

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 from turtle import Screen, Turtle
+import time
 screen = Screen()
 
-def create_screen() -> None:
-    screen.setup(width=600, height=600)
-    screen.bgcolor("black")
-    screen.title("Snake Arcade Game")
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Arcade Game")
+screen.tracer(0)
 
-def init_body() -> None:
-    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-    for position in starting_positions:
-        segment = Turtle(shape="square")
-        segment.color("white")
-        segment.goto(position)
+segments = []
+for position in starting_positions:
+    segment = Turtle(shape="square")
+    segment.color("white")
+    segment.penup()
+    segment.goto(position)
+    segments.append(segment)
 
-if __name__ == "__main__":
-    create_screen()
-    init_body()
-    screen.exitonclick()
+is_game_on = True
+
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+
+    for seg_num in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg_num - 1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(x=new_x, y=new_y)
+
+    segments[0].forward(20)
+
+
+screen.exitonclick()
 
