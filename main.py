33f3d09955a12edfacabba015e36d214b65b85e9
Drew Hund <languageinvestigator@gmail.com>
2026-03-23 12:43:11 -0400

    create snake body

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+from turtle import Screen, Turtle
+screen = Screen()
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def create_screen() -> None:
+    screen.setup(width=600, height=600)
+    screen.bgcolor("black")
+    screen.title("Snake Arcade Game")
 
+def init_body() -> None:
+    starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    for position in starting_positions:
+        segment = Turtle(shape="square")
+        segment.color("white")
+        segment.goto(position)
 
+if __name__ == "__main__":
+    create_screen()
+    init_body()
+    screen.exitonclick()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
